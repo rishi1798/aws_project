823cb4362420fa04d42f54a65a5f4188ee1de1f0
rishi1798 <rishi1798goyal@gmail.com>
2025-01-03 22:44:28 +0530

    new model

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from account.models import User
+from .models import Photo
 # Register your models here.
 
 admin.site.register(User)
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_at')
--- account/models.py
+++ account/models.py
@@ -36,3 +36,15 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"pk": self.id})
+
+
+from django.db import models
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='photos/')
+    description = models.TextField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title or f"Photo {self.id}"
