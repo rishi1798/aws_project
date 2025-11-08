64d5eee044d80651e80865f3c91be808480747ca
Rishigoyal-84 <rishi.member84@lakshmisri.com>
2025-11-08 22:19:37 +0530

    fix:commit from staging branch

--- account/models.py
+++ account/models.py
@@ -23,7 +23,7 @@ class User(AbstractUser):
     email = EmailField(_("email address"), unique=True)
     username = None  # type: ignore[assignment]
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "email1"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
